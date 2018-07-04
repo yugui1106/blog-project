009b2faff606ab188372476ca16eb16f7c73d75f
chengzw <chengzw@dxy.cn>
2018-07-04 23:39:02 +0800

    commit

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    class Meta:
+        ordering = ['-created_time']
---
+++ blog/views.py
@@ -0,0 +1,44 @@
+import markdown
+from comments.forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+
+
+# Create your views here.
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', context={
+        'post_list': post_list
+    })
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    )
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate)
+    return render(request, 'blog/index.html', context={'post_list': post_list})
